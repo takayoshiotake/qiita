d6669536135902cd986b5f249a3b5d33c765e4ac
OTAKE Takayoshi <takayoshiotake@users.noreply.github.com>
2017-04-23 21:50:11 +0900

    Fix: 時間計測間違ってる。

--- CとPythonでO_SYNCファイル書き込み/test_syncw.py
+++ CとPythonでO_SYNCファイル書き込み/test_syncw.py
@@ -12,12 +12,12 @@ COMMAND = b'COMMAND'
 def test_with_fileio():
     elapsed_time = -1
     with io.FileIO(DRIVER_FILE, mode='w') as file:
-        start = datetime.datetime.now()
+        start = time.time()
         for i in range(TEST_COUNT):
             file.seek(0)
             file.write(COMMAND)
             file.flush()
-        elapsed_time = (datetime.datetime.now() - start).microseconds
+        elapsed_time = (time.time() - start) * 1000000
         file.close()
     print(f'io.FileIO: {elapsed_time / TEST_COUNT}us')
 
@@ -25,12 +25,12 @@ def test_with_fileio():
 def test_with_iobase():
     elapsed_time = -1
     with open(DRIVER_FILE, mode='wb', buffering=0) as file:
-        start = datetime.datetime.now()
+        start = time.time()
         for i in range(TEST_COUNT):
             file.seek(0)
             file.write(COMMAND)
             file.flush()
-        elapsed_time = (datetime.datetime.now() - start).microseconds
+        elapsed_time = (time.time() - start) * 1000000
         file.close()
     print(f'io.IOBase: {elapsed_time / TEST_COUNT}us')
 
@@ -38,12 +38,12 @@ def test_with_iobase():
 def test_with_os():
     elapsed_time = -1
     fd = os.open(DRIVER_FILE, flags=(os.O_WRONLY | os.O_SYNC))
-    start = datetime.datetime.now()
+    start = time.time()
     for i in range(TEST_COUNT):
         os.lseek(fd, 0, os.SEEK_SET)
         os.write(fd, COMMAND)
     os.close(fd)
-    elapsed_time = (datetime.datetime.now() - start).microseconds
+    elapsed_time = (time.time() - start) * 1000000
     print(f'os: {elapsed_time / TEST_COUNT}us')
 
 
